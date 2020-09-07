23b2078969be57de9213939902370d0f08862c3f
Gabriel Carmo <70289493+GabrielCarmo-S@users.noreply.github.com>
2020-09-07 18:17:19 -0300

    Atualização da pagina home
    Arrumado o sidebar e navbar, carousel e cards;

--- style.js
+++ style.js
@@ -1,18 +1,47 @@
-$(document).ready(function () {
-    $('#sfc').on('click', function () {
 
-    	$(".sidebar").mCustomScrollbar({
-                theme: "minimal"
-        });
 
-    	$('#dismiss, .overlay').on('click', function () {
-              $('.sidebar').removeClass('active');
+$(document).ready(function () { 
+  $('#sfc').on('click', function () {
+
+
+  	$('.overlay').on('click', function () {
+              $('.mobileMenu').removeClass('active');
               $('.overlay').removeClass('active');
          });
 
-   		$('.sidebar').addClass('active');
-   		$('.overlay').addClass('active');
-   		 $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  	
+        $('.mobileMenu').toggleClass('active');
+        $('.overlay').toggleClass('active');
+       
+  });  
+});
 
-   	});
+	$(document).ready(function(){
+			  
+		$(".cos-card").click(function(){
+			$(".collapse1").collapse('show');
+		});
+
+		$(".co-card").click(function(){
+			$(".collapse2").collapse('show');
+		});
+			  
+	});
+
+$(document).ready(function () { 
+  $('.cos-card').on('click', function () {
+
+        $('#cd').addClass('active');
+        $('#cd2').removeClass('active');
+       
+  });  
+});
+
+$(document).ready(function () { 
+  $('.co-card').on('click', function () {
+
+        $('#cd2').addClass('active');
+        $('#cd').removeClass('active');
+      
+  });  
 });
