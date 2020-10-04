1e75edf0ac307fdbd1353a3e5e61db5d9828ff94
Gabriel <perien.op@gmail.com>
2020-10-03 21:15:11 -0300

    updates

--- application/views/adm/index.php
+++ application/views/adm/index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title>Home Admin</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="<?php echo base_url("public/css/style3.css"); ?>" rel="stylesheet">
+  <link href="<?php echo base_url("public/css/adm.css"); ?>" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 
